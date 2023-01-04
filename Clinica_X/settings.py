3224ad45d04326dbd27ee7fc12b22a71ae020368
walmir <walmir.pacheco34@gmail.com>
2023-01-04 17:51:24 -0300

    Projeto quarta parte

--- Clinica_X/settings.py
+++ Clinica_X/settings.py
@@ -128,5 +128,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MEDIA_URL = 'midia/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- core/views.py
+++ core/views.py
@@ -1,42 +1,50 @@
 from django.shortcuts import render
-from .forms import ClienteModelForm, FuncionarioModelForm
 from django.contrib import messages
+from django.shortcuts import redirect
+
+from .forms import ClienteModelForm, FuncionarioModelForm
 
 def index(request):
     return render(request, 'index.html')
 
 def Cad_Funcionario(request):
-    if str(request.method) == 'POST':
-        form = FuncionarioModelForm(request.POST, request.FILES)
-        if form.is_valid():
+    if str(request.user) != 'AnonymousUser':
+        if str(request.method) == 'POST':
+            form = FuncionarioModelForm(request.POST, request.FILES)
+            if form.is_valid():
 
-            form.save()
+                form.save()
             
-            messages.success(request, 'Funcionario salvo com sucesso!')
-            form = FuncionarioModelForm()
+                messages.success(request, 'Funcionario salvo com sucesso!')
+                form = FuncionarioModelForm()
+            else:
+                messages.error(request, 'Erro ao salvar cliente!')
         else:
-            messages.error(request, 'Erro ao salvar cliente!')
-    else:
             form = FuncionarioModelForm()
-    context = {
+        context = {
             'form': form
         }
-    return render(request, 'funcionario.html', context)    
+        return render(request, 'funcionario.html', context)
+    else:
+        return redirect('index')
 
 def Cad_Cliente(request):
-    if str(request.method) == 'POST':
-        form = ClienteModelForm(request.POST, request.FILES)
-        if form.is_valid():
+    if str(request.user) != 'AnonymousUser':
+        if str(request.method) == 'POST':
+            form = ClienteModelForm(request.POST, request.FILES)
+            if form.is_valid():
 
-            form.save()
+                form.save()
 
-            messages.success(request, 'Cliente salvo com sucesso!')
-            form = ClienteModelForm()
+                messages.success(request, 'Cliente salvo com sucesso!')
+                form = ClienteModelForm()
+            else:
+                messages.error(request, 'Erro ao salvar cliente!')
         else:
-            messages.error(request, 'Erro ao salvar cliente!')
-    else:
             form = ClienteModelForm()
-    context = {
+        context = {
             'form': form
         }
-    return render(request, 'cliente.html', context)    
+        return render(request, 'cliente.html', context)
+    else:
+        return redirect('index')  
